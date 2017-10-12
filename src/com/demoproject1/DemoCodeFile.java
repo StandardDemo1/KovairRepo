503edc69e0e8adf5b7fea6f55ed795468bb29213
rs267 <rs267@demo1.sr.kovair.com>
2017-10-12 03:00:30 -0700

    4

--- src/com/demoproject1/DemoCodeFile.java
+++ src/com/demoproject1/DemoCodeFile.java
@@ -3,6 +3,7 @@ package com.demoproject1;
 public class DemoCodeFile {
 	public static void main(String []args){
 		System.out.println("DemoCodeFile");
+		System.out.println("DemoCodeFile11");
  	}
 
 }
