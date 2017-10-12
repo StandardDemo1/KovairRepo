80e1ec8362d4deee53d2ca1968c2600f17683d11
rs267 <rs267@demo1.sr.kovair.com>
2017-10-12 05:35:27 -0700

    5

---
+++ src/com/demoproject1/DemoCodeFile3.java
@@ -0,0 +1,10 @@
+package com.demoproject1;
+
+public class DemoCodeFile3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Kovair Demo Project");
+	}
+
+}
