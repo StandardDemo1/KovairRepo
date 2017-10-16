3eef9cd5c84c571afb8691e6c18cee2a2ccefc47
rs267 <rs267@demo1.sr.kovair.com>
2017-10-16 04:31:57 -0700

    17

--- src/com/demoproject1/DemoCodeFile.java
+++ src/com/demoproject1/DemoCodeFile.java
@@ -2,7 +2,7 @@ package com.demoproject1;
 
 public class DemoCodeFile {
 	public static void main(String []args){
-		System.out.println("DemoCodeFile Modified v1");
+		System.out.println("DemoCodeFile Modified v17");
  	}
 
 }
