b6894be7510b017d6cb8227bc6b517fd29367f25
mn260 <mn260@demo1.sr.kovair.com>
2017-11-06 04:34:15 -0800

    1

--- StandardDemo3/src/Demo3/CodeFile1.java
+++ StandardDemo3/src/Demo3/CodeFile1.java
@@ -5,7 +5,7 @@ public class CodeFile1 {
 	public static void main(String[] args) {
 		
 		//Hello Comment
-		System.out.println("File1 modified");
+		System.out.println("File1 modified 2");
 	}
 
 }
