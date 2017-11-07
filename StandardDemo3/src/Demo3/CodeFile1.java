296157b3bf9b9107013016a9c1d57487fa5fd6e2
rs267 <rs267@demo1.sr.kovair.com>
2017-11-06 22:02:09 -0800

    1132

--- StandardDemo3/src/Demo3/CodeFile1.java
+++ StandardDemo3/src/Demo3/CodeFile1.java
@@ -5,7 +5,7 @@ public class CodeFile1 {
 	public static void main(String[] args) {
 		
 		//Hello Comment
-		System.out.println("File1 modified 3");
+		System.out.println("File1 mod for Add Users page 3");
 	}
 
 }
