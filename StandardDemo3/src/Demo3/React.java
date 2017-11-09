f2f0788fd96d6f437faf1c298cde4ab88e319bf4
mn260 <mn260@demo1.sr.kovair.com>
2017-11-08 20:49:38 -0800

    4

--- StandardDemo3/src/Demo3/React.java
+++ StandardDemo3/src/Demo3/React.java
@@ -1,10 +1,39 @@
 package Demo3;
+import java.sql.*;
+import javax.sql.*;
+import javax.naming.*;
+
 
 public class React {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	
+		        Connection con;
+		        PreparedStatement pstmt;
+		        try {
+		           
+		           con.setAutoCommit(true);
+		            pstmt = con.prepareStatement("UPDATE COFFEES " +
+		                        "SET PRICE = ? " +
+		                        "WHERE COF_NAME = ?");
+		            pstmt.setFloat(1, price);
+		            pstmt.setString(2, cofName);
+		            pstmt.executeUpdate();
+
+		            con.commit();
+		            pstmt.close();
+
+		        } finally {
+		            if (con != null) con.close();
+		        }
+		    }
+
+		    private DataSource ds = null;
+		    private Context ctx = null;
+		}
+
 
 	}
 
-}
+
