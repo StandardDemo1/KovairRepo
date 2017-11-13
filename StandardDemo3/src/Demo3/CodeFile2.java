9df8c83564d25e6af8c96c14062b17517b306844
rs267 <rs267@demo1.sr.kovair.com>
2017-11-13 03:37:37 -0800

    0

--- StandardDemo3/src/Demo3/CodeFile2.java
+++ StandardDemo3/src/Demo3/CodeFile2.java
@@ -7,7 +7,7 @@ public class CodeFile2 {
 		    int a[]=new int[5];  
 		    a[5]=30/0;  
 		   }  
-		   catch(ArithmeticException e){System.out.println("task1 is completed");}  
+		   catch(ArithmeticException e){System.out.println("task1 is completed mod1");}  
 		   catch(ArrayIndexOutOfBoundsException e){System.out.println("task 2 completed");}  
 		   catch(Exception e){System.out.println("common task completed");}  
 		  
