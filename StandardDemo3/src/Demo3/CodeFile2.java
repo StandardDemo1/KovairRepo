e9291d9e07f18eb7cf9df14b5f478bd6b3ad42db
rs267 <rs267@demo1.sr.kovair.com>
2017-11-14 07:01:49 -0800

    1199

--- StandardDemo3/src/Demo3/CodeFile2.java
+++ StandardDemo3/src/Demo3/CodeFile2.java
@@ -5,7 +5,7 @@ public class CodeFile2 {
 	public static void main(String[] args) {
 		try{  
 		    int a[]=new int[5];  
-		    a[5]=30/0;  // Code for Requirement 1132
+		    a[5]=30/0;  // Code for Requirement 1199
 		   }  
 		   catch(ArithmeticException e){System.out.println("task1 is completed mod12");}  
 		   catch(ArrayIndexOutOfBoundsException e){System.out.println("task 2 completed");}  
