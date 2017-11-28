1abb553c0571e916f1e1332f9e5a336fdb851a2e
rs267 <rs267@demo1.sr.kovair.com>
2017-11-27 22:15:18 -0800

    44

--- StandardDemo3/src/Demo3/CodeFile4.java
+++ StandardDemo3/src/Demo3/CodeFile4.java
@@ -4,7 +4,7 @@ public class CodeFile4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Demo Code 4");
+		System.out.println("Demo Code 4 Mod");
 	}
 
 }
