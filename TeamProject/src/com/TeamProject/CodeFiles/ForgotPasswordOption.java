d8f7f9f34e162224e69cb2cb79405bfd46662753
rs267 <rs267@demo1.sr.kovair.com>
2017-12-21 04:04:55 -0800

    US2202

--- TeamProject/src/com/TeamProject/CodeFiles/ForgotPasswordOption.java
+++ TeamProject/src/com/TeamProject/CodeFiles/ForgotPasswordOption.java
@@ -1,10 +1,41 @@
 package com.TeamProject.CodeFiles;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class ForgotPasswordOption {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		//US2202
+		String s1;
+	    String s2;
+
+	    double num1, num2, product;
+
+	    // set up the buffered reader to read from the keyboard
+	    BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+
+	    System.out.println ("Enter a line of input");
+	    
+	    /* Following line triggers the error.  Error will show the type of
+	       unhandled exception and where the call occurs */
+	    s1 = br.readLine();
+
+	    System.out.println ("The line has " + s1.length() + " characters");
+
+	    System.out.println ();
+	    System.out.println ("Breaking the line into tokens we get:");
+
+	    int numTokens = 0;
+	    StringTokenizer st = new StringTokenizer (s1);
+
+	    while (st.hasMoreTokens()) {
+		s2 = st.nextToken();
+		numTokens++;
+		System.out.println ("    Token " + numTokens + " is: " + s2); //US2208
+	    }
 	}
 
 }
