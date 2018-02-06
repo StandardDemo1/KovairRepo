0364a33dcf4e32d3109cca65cbc5880f34f8ddc4
Rohan <rohansaxena02@gmail.com>
2018-02-06 10:21:23 +0530

    1239

--- ProjectStandardDemo1/src/com/standarddemo1/codefiles/RegistrationPage.java
+++ ProjectStandardDemo1/src/com/standarddemo1/codefiles/RegistrationPage.java
@@ -3,8 +3,9 @@ package com.standarddemo1.codefiles;
 public class RegistrationPage {
 
 	public static void main(String[] args) {
-		////// TODO Auto-generated method stub
-		///////// ID=1239
+		// TODO Auto-generated method stub
+		//File Modified 1
+		// ID=1239
 	}
 
 }
